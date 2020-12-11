21bc2d819f27206a8f9d86ed547400170f89b3be
LudoLerma <lermaludovica@gmail.com>
2020-12-11 02:14:15 +0100

    servlets checklogin, cheksingup

--- src/gamifyUser/controllers/CheckLogin.java
+++ src/gamifyUser/controllers/CheckLogin.java
@@ -1,26 +1,77 @@
 package gamifyUser.controllers;
+
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-
+import javax.ejb.EJB;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.annotation.MultipartConfig;
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringEscapeUtils;
+import polimi.db2.gamifyDB.services.UserService;
+import polimi.db2.gamifyDB.entities.User;
+import javax.persistence.NonUniqueResultException;
 
 
 @WebServlet("/CheckLogin")
 @MultipartConfig
 public class CheckLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Connection connection = null;
+	@EJB(name = "polimi.db2.gamifyDB.services/UserService")
+	private UserService usrService;
 
 	public CheckLogin() {
 		super();
 	}
 
+	public void init() throws ServletException {
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// obtain and escape params
+		String usrn = null;
+		String pwd = null;
+		try {
+			usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+			if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty()) {
+				throw new Exception("Missing or empty credential value");
+			}
+
+		} catch (Exception e) {
+			// for debugging only e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
+			return;
+		}
+		User user;
+		try {
+			// query db to authenticate for user
+			user = usrService.checkCredentials(usrn, pwd);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not check credentials");
+			return;
+		}
+
+		// If the user exists, add info to the session and go to home page, otherwise
+		// show login page with error message
+		if (user == null) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().println("Incorrect credentials");
+		} else {
+			request.getSession().setAttribute("user", user);
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().println(usrn);
+		}
+	}
+
+	public void destroy() {
+	}
 }
 
