95547f75d5bf841532ab47903aedd546cec776b3
darklamp <alessandro17.villa@mail.polimi.it>
2020-12-12 17:42:48 +0100

    updated

--- src/gamifyUser/controllers/CheckSignUp.java
+++ src/gamifyUser/controllers/CheckSignUp.java
@@ -1,6 +1,11 @@
 package gamifyUser.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import polimi.db2.gamifyDB.entities.User;
 import polimi.db2.gamifyDB.services.UserService;
 
 import javax.ejb.EJB;
@@ -12,9 +17,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringEscapeUtils;
 
+import gamifyUser.utility.Utility;
+
 @WebServlet("/CheckSignUp")
 @MultipartConfig
 public class CheckSignUp extends HttpServlet {
+	
+	public static final int MAX_USERNAME_LENGTH = 40;
+	public static final int MAX_EMAIL_LENGTH = 100;
+	public static final int MAX_PWD_LENGTH = 100;
+
 	private static final long serialVersionUID = 198237498L;
 	@EJB(name = "polimi.db2.gamifyDB.services/UserService")
 	private UserService usrService;
@@ -29,41 +41,50 @@ public class CheckSignUp extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// obtain and escape params
-		String usrn = null;
-		String pwd = null;
-		String pwd2 = null;
-		usrn = StringEscapeUtils.escapeJava(request.getParameter("user2"));
-		pwd = StringEscapeUtils.escapeJava(request.getParameter("pass2"));
-		pwd2 = StringEscapeUtils.escapeJava(request.getParameter("pass3"));
-
-		if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty() || pwd2.isEmpty()|| pwd2 == null) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.getWriter().println("Credentials must be not null");
-			return;
-		}
-		if (!pwd2.contentEquals(pwd)) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.getWriter().println("Uuups something went wrong!");
+		String usrn, pwd, pwd2, email;
+		
+		List<String> mandatoryParams = new ArrayList<>(Arrays.asList("user2", "mail2", "pass2", "pass3"));
+		if(!Utility.paramExists(request, response, mandatoryParams) || Utility.paramIsEmpty(request, response, mandatoryParams)) return;
+		
+		try {
+			usrn = StringEscapeUtils.escapeJava(request.getParameter("user2"));
+			email = StringEscapeUtils.escapeJava(request.getParameter("mail2"));
+			if(!Utility.isValidMailAddress(email)) throw new Exception();
+			pwd = StringEscapeUtils.escapeJava(request.getParameter("pass2"));
+			pwd2 = StringEscapeUtils.escapeJava(request.getParameter("pass3"));
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request");
 			return;
 		}
 		
-		//TODO metodi per controllare il signup
-		UserService user = new UserService();
+		if (!pwd2.contentEquals(pwd)) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request. Passwords do not match.");
+			return;
+		}
+		
+		if (usrn.length() >= MAX_USERNAME_LENGTH || email.length() > MAX_EMAIL_LENGTH || pwd.length() > MAX_PWD_LENGTH) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request. Passwords do not match.");
+			return;
+		}
+		
+		User user;
 		try {
-			user.createUser(usrn, pwd, usrn);
+			user = usrService.createUser(usrn, pwd, email);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error. Please try again later.");
+			return;
+		}
+		if(user == null) {
+			response.setStatus(HttpServletResponse.SC_CONFLICT);
+			response.getWriter().println("Incorrect credentials");
+			return;
 		}
 	
-			
-
 				request.getSession().setAttribute("user", usrn);
 				response.setStatus(HttpServletResponse.SC_OK);
 				response.setContentType("application/json");
 				response.setCharacterEncoding("UTF-8");
 				response.getWriter().println(usrn);
-				
 		
   }
 	
